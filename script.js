df1851a51f6eabadadaf5f9d75d4ab8fe7b06e99
Waza80 <108598100+Waza80@users.noreply.github.com>
2024-03-31 11:26:22 +0200

    Use websocket for constant status update Clean up console Fix status bug

--- script.js
+++ script.js
@@ -64,24 +64,20 @@ function updateTime(element, timestamp) {
     })
 }
 
-fetch("https://api.lanyard.rest/v1/users/959534223293833256", {
-    method: "GET"
-})
-    .then(response => response.json())
-    .then(data => {
-        if (data.success !== true) {return}
-
+let firstTime = true
+function updateStatus(data) {
+    if (firstTime) {
+        firstTime = false
         let c = document.querySelector(".img-placeholder-1"), 
-            d = document.querySelector(".img-placeholder-2"),
-            e = document.querySelector(".dsc-display-name"),
-            f = document.querySelector(".dsc-username")
+            d = document.querySelector(".img-placeholder-2");
+
         let img = document.createElement("img");
         img.id = "center-image";
         img.alt = "Waza Icon";
         img.width = "150";
         img.height = "150";
         img.src = "https://cdn.discordapp.com/avatars/959534223293833256/"
-            + data.data.discord_user.avatar 
+            + data.discord_user.avatar 
             + ".webp?size=512";
         c.replaceWith(img)
 
@@ -92,59 +88,89 @@ fetch("https://api.lanyard.rest/v1/users/959534223293833256", {
         img2.width = "128";
         img2.height = "128";
         img2.src = "https://cdn.discordapp.com/avatars/959534223293833256/"
-            + data.data.discord_user.avatar 
+            + data.discord_user.avatar 
             + ".webp?size=256";
         d.replaceWith(img2)
+    }
 
-        e.textContent = data.data.discord_user.global_name
-        f.textContent = data.data.discord_user.username
+    let e = document.querySelector(".dsc-display-name"),
+        f = document.querySelector(".dsc-username")
 
-        const noActivity = document.getElementById("no-activity")
-        const activityContainer = document.getElementById("activity-container")
-        const firstActivity = document.getElementById("activity")
-        if (data.data.activities.length > 0) {
-            noActivity.remove()
-            data.data.activities.forEach(activity => {
-                let a = firstActivity.cloneNode(true),
-                    a_img = a.querySelector("#image")
-                    a_label = a.querySelector("#label-container")
-                
-                a.classList = ""
-                
-                if (activity.name !== "Spotify") {
-                    if (activity.assets != undefined && activity.assets.large_image != undefined) {
-                        a_img.src = activity.assets.large_image.startsWith("mp:external/")
-                            ? `https://media.discordapp.net/external/${activity.assets.large_image.replace("mp:external/", "")}` 
-                            : `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activity.assets.large_image}.webp`
-                    }
+    e.textContent = data.discord_user.global_name
+    f.textContent = data.discord_user.username
 
-                    if (activity.name != undefined) {a_label.querySelector("#title").textContent = activity.name} else {a_label.querySelector("#title").remove()}
-                    if (activity.details != undefined) {a_label.querySelector(".details").textContent = activity.details} else {a_label.querySelector(".details").remove()}
-                    if (activity.state != undefined) {a_label.querySelector(".state").textContent = activity.state} else {a_label.querySelector(".state").remove()}
-                    if (activity.timestamps != undefined && activity.timestamps.start != undefined) {updateTime(a_label.querySelector(".timestamp"), activity.timestamps.start)} else {a_label.querySelector(".timestamp").remove()}
-                } else {
-                    if (data.data.spotify.album_art_url != undefined) {
-                        a_img.src = data.data.spotify.album_art_url
-                    }
-                    
-                    if (activity.name != undefined) {a_label.querySelector("#title").textContent = activity.name} else {a_label.querySelector("#title").remove()}
-                    if (activity.details != undefined) {a_label.querySelector(".details").textContent = activity.details} else {a_label.querySelector(".details").remove()}
-                    if (activity.state != undefined) {a_label.querySelector(".state").textContent = activity.state} else {a_label.querySelector(".state").remove()}
-                    if (data.data.spotify.timestamps.start) {updateTime(a_label.querySelector(".timestamp"), data.data.spotify.timestamps.start)} else {a_label.querySelector(".timestamp").remove()}
-                }
-                activityContainer.appendChild(a)
-            })
-            activityContainer.querySelector("#activity").remove()
-            if (activityContainer.childElementCount == 0) {
-                activityContainer.remove()
-            }
-        } else {
-            activityContainer.remove()
-            noActivity.classList = ""
+    const noActivity = document.getElementById("no-activity")
+    const activityContainer = document.getElementById("activity-container")
+    const firstActivity = document.getElementById("activity")
+
+    document.querySelectorAll("#activity").forEach((item) => {
+        if (item.classList.contains("hidden") != true) {
+            item.remove()
         }
     })
 
+    if (data.activities.length > 0) {
+        noActivity.classList.add("hidden")
+        activityContainer.className = ""
+        data.activities.forEach(activity => {
+            let a = firstActivity.cloneNode(true),
+                a_img = a.querySelector("#image"),
+                a_label = a.querySelector("#label-container");
+            a.classList = ""
+
+            if (activity.name !== "Spotify") {
+                if (activity.assets != undefined && activity.assets.large_image != undefined) {
+                    a_img.src = activity.assets.large_image.startsWith("mp:external/")
+                        ? `https://media.discordapp.net/external/${activity.assets.large_image.replace("mp:external/", "")}` 
+                        : `https://cdn.discordapp.com/app-assets/${activity.application_id}/${activity.assets.large_image}.webp`
+                }
+                if (activity.name != undefined) {a_label.querySelector("#title").textContent = activity.name} else {a_label.querySelector("#title").remove()}
+                if (activity.details != undefined) {a_label.querySelector(".details").textContent = activity.details} else {a_label.querySelector(".details").remove()}
+                if (activity.state != undefined) {a_label.querySelector(".state").textContent = activity.state} else {a_label.querySelector(".state").remove()}
+                if (activity.timestamps != undefined && activity.timestamps.start != undefined) {updateTime(a_label.querySelector(".timestamp"), activity.timestamps.start)} else {a_label.querySelector(".timestamp").remove()}
+            } else {
+                    if (data.spotify.album_art_url != undefined) {
+                    a_img.src = data.spotify.album_art_url
+                }
+                
+                if (activity.name != undefined) {a_label.querySelector("#title").textContent = activity.name} else {a_label.querySelector("#title").remove()}
+                if (activity.details != undefined) {a_label.querySelector(".details").textContent = activity.details} else {a_label.querySelector(".details").remove()}
+                if (activity.state != undefined) {a_label.querySelector(".state").textContent = activity.state} else {a_label.querySelector(".state").remove()}
+                if (data.spotify.timestamps.start) {updateTime(a_label.querySelector(".timestamp"), data.spotify.timestamps.start)} else {a_label.querySelector(".timestamp").remove()}
+            }
+            activityContainer.appendChild(a)
+        })
+    } else {
+        activityContainer.classList.add("hidden")
+        noActivity.className = ""
+    }
+}
+
 let ws = new WebSocket("wss://api.lanyard.rest/socket")
+ws.onerror = () => {console.warn("Websocket connection failed, status auto-update is unavailable")}
+
+let heartbeatInterval = null
+ws.onmessage = (event) => {
+    let event_data = JSON.parse(event.data)
+    if (event_data.op === 1) {
+        heartbeatInterval = event_data.d.heartbeat_interval
+        setInterval(() => {ws.send(JSON.stringify({op: 3})); console.log("Heartbeat sent")}, heartbeatInterval)
+        ws.send(JSON.stringify({
+            op: 2,
+            d: {
+                subscribe_to_ids: ["959534223293833256"],
+            }
+        }))
+    } else if (event_data.op === 0) {
+        if (event_data.t === "INIT_STATE") {
+            updateStatus(event_data.d[Object.keys(event_data.d)[0]])
+        } else {
+            updateStatus(event_data.d)
+        }
+    } else if (event_data.op === 3) {
+        // console.log(event_data.d)
+    }
+}
 
 const canvas = document.getElementById("particles-container");
 const ctx = canvas.getContext("2d");
