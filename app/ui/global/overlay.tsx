14666b33ea2647e98489669adb7fe373003b7102
Waza80 <wyattbirac3@gmail.com>
2025-05-18 23:49:47 +0200

    Fix overlay on mobile

--- app/ui/global/overlay.tsx
+++ app/ui/global/overlay.tsx
@@ -1,6 +1,6 @@
 import { DiscordData, Spotify } from "@/app/page"
 import clsx from "clsx"
-import { LoaderCircle, X } from "lucide-react"
+import { LoaderCircle } from "lucide-react"
 import Image from "next/image"
 import { useState, useEffect } from "react"
 
@@ -13,30 +13,19 @@ const formatTime = (seconds: number) => {
     : `${mins}:${secs.toString().padStart(2, '0')}`
 }
 
-function useSpotifyProgress(start?: number, end?: number) {
+function useTimeUpdater() {
   const [progress, setProgress] = useState(0)
 
   useEffect(() => {
-    if (!start || !end) return
-
     let animationFrame: number
-    const duration = end - start
-    
     const updateProgress = () => {
-      const now = Date.now()
-      const elapsed = now - start
-      const newProgress = Math.min(elapsed, duration)
-      
-      setProgress(newProgress)
-      
-      if (elapsed < duration) {
-        animationFrame = requestAnimationFrame(updateProgress)
-      }
+      setProgress(Date.now())
+      animationFrame = requestAnimationFrame(updateProgress)
     }
 
     updateProgress()
     return () => cancelAnimationFrame(animationFrame)
-  }, [start, end])
+  }, [])
 
   return progress
 }
@@ -52,10 +41,10 @@ export function Overlay({
 }) {
   const state = data as DiscordData
   const spotify = state.spotify as Spotify
-  const progress = useSpotifyProgress(spotify?.timestamps?.start, spotify?.timestamps?.end)
+  const now = useTimeUpdater()
 
   return (
-    <div className="fixed z-50 w-screen h-screen p-4 gap-2 flex flex-col justify-end items-end">
+    <div className="fixed bottom-0 right-0 z-50 p-4 gap-2 flex flex-col justify-end items-end">
       <div className={clsx(
         "size-10 bg-blue-400 rounded-full appear flex justify-center items-center transition-all duration-200",
         {
@@ -71,7 +60,6 @@ export function Overlay({
         }
       )}>
         Disconnected, reload the page.
-        <X/>
       </div>
       <div className={clsx(
         "transition-all duration-200 appear",
@@ -84,7 +72,7 @@ export function Overlay({
       {state?.activities && state.activities.map(activity => {
         if (activity.name === 'Spotify') return
         return (
-          <div key={activity.id} className="w-76 max-w-full p-1 bg-slate-900 rounded-full appear flex gap-2 items-center">
+          <div key={activity.id} className="max-w-[min(100%,392px)] p-1 pr-6 bg-slate-900 rounded-full appear flex gap-2 items-center">
             <Image
               src={activity.assets?.large_image ? (activity.assets.large_image.startsWith("mp:external/")
                 ? `https://media.discordapp.net/external/${activity.assets.large_image.replace("mp:external/", "")}` 
@@ -108,7 +96,7 @@ export function Overlay({
       {state.listening_to_spotify && (
         <a href={"https://open.spotify.com/track/" + spotify?.track_id} className="relative overflow-clip w-98 max-w-full bg-[#006000] rounded-2xl appear transition-all duration-200">
           <div className="absolute bg-blue-200/20 h-full top-0 left-0" style={{
-            width: (((spotify.timestamps.start + progress) - spotify.timestamps.start) / (spotify.timestamps.end - spotify.timestamps.start) * 100) + '%'
+            width: ((now - spotify.timestamps.start) / (spotify.timestamps.end - spotify.timestamps.start) * 100) + '%'
           }}></div>
           <div className="relative size-full flex gap-1 p-1">
             <Image
@@ -118,17 +106,17 @@ export function Overlay({
               className="rounded-xl select-none pointer-events-none"
               alt={spotify.album}
             />
-            <div className="flex select-none pointer-events-none flex-col justify-center">
-              <p className="truncate -my-0.5">
-                {spotify.song}
+            <div className="flex flex-1 min-w-0 select-none pointer-events-none flex-col justify-center">
+              <p className="truncate max-w-full -my-0.5">
+          {spotify.song}
               </p>
-              <p className="text-sm select-none pointer-events-none truncate text-gray-400 -my-0.5">
-                {spotify.artist}
+              <p className="text-sm select-none pointer-events-none max-w-full truncate text-gray-400 -my-0.5">
+          {spotify.artist}
               </p>
             </div>
-            <div className="flex-1 flex justify-end items-center pr-2">
+            <div className="flex-none flex justify-end items-center pr-2">
               <p className="text-sm select-none pointer-events-none text-center">
-              {`${formatTime(progress / 1000)} / ${formatTime((spotify.timestamps.end - spotify.timestamps.start) / 1000)}`}
+              {`${formatTime((now - spotify.timestamps.start) / 1000)} / ${formatTime((spotify.timestamps.end - spotify.timestamps.start) / 1000)}`}
               </p>
             </div>
           </div>
